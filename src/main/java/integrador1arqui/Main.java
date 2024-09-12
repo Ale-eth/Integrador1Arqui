f7e77ffd93bda068e7992a6f2e59badc33f19449
Ale <alejandroabdala2000@gmail.com>
2024-09-11 22:21:03 -0300

    Entrega

---
+++ src/main/java/integrador1arqui/Main.java
@@ -0,0 +1,51 @@
+package integrador1arqui;
+
+import integrador1arqui.daos.ClienteMysqlDAO;
+import integrador1arqui.daos.FacturaProductoMysqlDAO;
+import integrador1arqui.dtos.RecaudacionDTO;
+import integrador1arqui.entities.Cliente;
+import integrador1arqui.factory.AbstractFactory;
+import integrador1arqui.utils.HelperMySQL;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+
+        System.out.println("////////////////////////////////////////////");
+        System.out.println("1- Cree un programa utilizando JDBC que cree el esquema de la base de datos: ");
+        HelperMySQL dbMySQL = new HelperMySQL();
+        dbMySQL.dropTables();
+        dbMySQL.createTables();
+        System.out.println("////////////////////////////////////////////");
+
+        System.out.println();
+
+        System.out.println("////////////////////////////////////////////");
+        System.out.println("2- Considere los CSV dados y escriba un programa JDBC que cargue los datos a la base de datos: ");
+        dbMySQL.populateDB();
+        dbMySQL.closeConnection();
+        System.out.println("////////////////////////////////////////////");
+
+        // Factory 1: MySQL
+        AbstractFactory chosenFactory = AbstractFactory.getDAOFactory(1);
+        ClienteMysqlDAO clienteMysqlDAO = chosenFactory.getClienteMysqlDAO();
+        FacturaProductoMysqlDAO facturaProductoMysqlDAO = chosenFactory.getFacturaProductoMysqlDAO();
+
+        System.out.println("");
+
+        System.out.println("////////////////////////////////////////////");
+        System.out.println("3- Escriba un programa JDBC que retorne el producto que más recaudó. Se define recaudación” como cantidad de productos vendidos multiplicado por su valor: ");
+        RecaudacionDTO recaudacionDTO = facturaProductoMysqlDAO.getRecaudacion();
+        System.out.println(recaudacionDTO);
+        System.out.println("////////////////////////////////////////////");
+
+        System.out.println("");
+
+        System.out.println("////////////////////////////////////////////");
+        System.out.println("4- Escriba un programa JDBC que imprima una lista de clientes, ordenada por a cuál se le facturó más: ");
+        List<Cliente> listadoClientes = clienteMysqlDAO.selectList();
+        System.out.println(listadoClientes);
+
+    }
+}
---
+++ src/main/java/integrador1arqui/entities/ClienteFacturacion.java
@@ -0,0 +1,24 @@
+package integrador1arqui.entities;
+
+public class ClienteFacturacion extends Cliente {
+    private float facturacion;
+
+    public ClienteFacturacion(int id, String nombre, String email, float fact) {
+        super(id, nombre, email);
+        this.facturacion=fact;
+
+    }
+
+    public float getFacturacion() {
+        return facturacion;
+    }
+
+    public void setFacturacion(float facturacion) {
+        this.facturacion = facturacion;
+    }
+
+    @Override
+    public String toString() {
+        return "Id:" + this.getIdCliente() + " " + "Nombre:" + this.getNombre() + " " + "Email:" + this.getEmail()+ ", facturacion=" + facturacion ;
+    }
+}
